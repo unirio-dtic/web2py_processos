638a425e1a2b424bd2752352b62a2c9faccca3f8
diogomartins <diogo.martins@unirio.br>
2015-03-26 13:28:30 -0300

    Refatorações

--- models/menu.py
+++ models/menu.py
@@ -1,10 +1,4 @@
 # -*- coding: utf-8 -*-
-# this file is released under public domain and you can use without limitations
-
-#########################################################################
-## Customize your APP title, subtitle and menus here
-#########################################################################
-
 response.logo = A("DTIC/UNIRIO",
                   _class="brand",_href="http://www.unirio.br/dtic")
 response.title = request.application.replace('_',' ').title()
